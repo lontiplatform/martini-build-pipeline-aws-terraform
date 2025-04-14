2a7be4cc9f55a6e7285b43c1d81891d6270585b8
oss-ubuntu <jasonquinto.it@gmail.com>
2025-04-14 20:06:54 +0800

    Fixed formatting

--- martini-upload-package/ssm.tf
+++ martini-upload-package/ssm.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "martini_upload_package" {
   type = "SecureString"
   value = jsonencode({
     BASE_URL              = var.base_url
-    MARTINI_ACCESS_TOKEN     = var.martini_access_token
+    MARTINI_ACCESS_TOKEN  = var.martini_access_token
   })
   description = "SSM parameter for Martini build image configurations"
   tags = {
